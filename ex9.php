45bf412748920a2e59534b3dece897cdfbdd1a32
Yassin Azzouz <serjazouz@gmail.com>
2023-10-04 22:43:30 +0100

    ex9

---
+++ ex9.php
@@ -0,0 +1,12 @@
+<?php
+    $a=15;
+    if ($a%3==0 && $a%5==0)
+    {
+        echo "$a est multiple de 3 et 5";
+    }
+    else 
+    {
+        echo "$a n'est multiple un multiple de ce deux nombres";
+
+    }
+
