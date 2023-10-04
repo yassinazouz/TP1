bf9fbbfbd3d98161f7b2b805028dd9a86d99ca21
Yassin Azzouz <serjazouz@gmail.com>
2023-10-04 17:50:44 +0100

    EX1

---
+++ ex1.php
@@ -0,0 +1,7 @@
+<?php
+    $ABC ="C'est la variable \$ABC";
+    $_VAR="C'est la variable \$_VAR";
+    $_1VAR ="C'est la variable \$_1VAR";
+    $prénom = "C'est la variable \$prénom";
+    echo "Les variables valides sont \$ABC \$_VAR \$_1VAR \$prénom </br>";
+    echo $ABC. "</br>" .$_VAR. "</br>" .$_1VAR. "</br>" .$prénom;
